1bb7cf9b7fcb35fdd90a9f1989fecc00f515f66c
endrix <endrix@d5e41471-06c4-453d-a0e6-8aee366141c0>
2012-03-12 09:13:49 +0000

    (OpenForge) Changing from OpenForge to net.sf.openforge

--- eclipse/plugins/net.sf.openforge/source/java/net/sf/openforge/Activator.java
+++ eclipse/plugins/net.sf.openforge/source/java/net/sf/openforge/Activator.java
@@ -1,4 +1,4 @@
-package openforge;
+package net.sf.openforge;
 
 import org.eclipse.core.runtime.Plugin;
 import org.osgi.framework.BundleContext;
@@ -9,7 +9,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends Plugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "OpenForge";
+	public static final String PLUGIN_ID = "net.sf.openforge";
 
 	// The shared instance
 	private static Activator plugin;
