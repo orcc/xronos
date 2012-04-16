7709c01230ab9fe5be932c88f9c86be2c43704c4
mraulet <mraulet@d5e41471-06c4-453d-a0e6-8aee366141c0>
2012-04-16 20:10:23 +0000

    Comment multi2mono pass

--- eclipse/plugins/net.sf.orc2hdl/src/net/sf/orc2hdl/backend/Orc2HDL.java
+++ eclipse/plugins/net.sf.orc2hdl/src/net/sf/orc2hdl/backend/Orc2HDL.java
@@ -345,7 +345,7 @@ public class Orc2HDL extends AbstractBackend {
 		actor.setTemplateData(data);
 
 		DfSwitch<?>[] transformations = { new StoreOnceTransformation(),
-				new LocalArrayRemoval(), new Multi2MonoToken(),
+				new LocalArrayRemoval(), //new Multi2MonoToken(),
 				new DivisionSubstitution(), new UnitImporter(),
 				new SSATransformation(),
 				/* new TypeResizer(false, true, true, true), */
