cd46f7c703e418753299b9db52858bacc1ed8700
hyviquel <hyviquel@d5e41471-06c4-453d-a0e6-8aee366141c0>
2012-05-23 09:28:16 +0000

    Rename 'transformations' package in 'tranform' (to make easier the navigation on small laptop screen).

--- eclipse/plugins/net.sf.orc2hdl/src/net/sf/orc2hdl/backend/transform/IndexFlattener.java
+++ eclipse/plugins/net.sf.orc2hdl/src/net/sf/orc2hdl/backend/transform/IndexFlattener.java
@@ -26,7 +26,7 @@
  * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
  */
-package net.sf.orc2hdl.backend.transformations;
+package net.sf.orc2hdl.backend.transform;
 
 import java.util.ArrayList;
 import java.util.List;
