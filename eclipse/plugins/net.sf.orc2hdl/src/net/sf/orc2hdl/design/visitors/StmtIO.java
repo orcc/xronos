d24406b7826f5739c1f94714ffb1528f6e5c84a2
endrix <endrix@d5e41471-06c4-453d-a0e6-8aee366141c0>
2012-09-12 03:48:25 +0000

    (Orc2HDL) InstanceToDesign, Adding licensing and comments

--- eclipse/plugins/net.sf.orc2hdl/src/net/sf/orc2hdl/design/visitors/StmtIO.java
+++ eclipse/plugins/net.sf.orc2hdl/src/net/sf/orc2hdl/design/visitors/StmtIO.java
@@ -1,3 +1,31 @@
+/*
+ * Copyright (c) 2012, Ecole Polytechnique Fédérale de Lausanne
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ *   * Redistributions of source code must retain the above copyright notice,
+ *     this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright notice,
+ *     this list of conditions and the following disclaimer in the documentation
+ *     and/or other materials provided with the distribution.
+ *   * Neither the name of the Ecole Polytechnique Fédérale de Lausanne nor the names of its
+ *     contributors may be used to endorse or promote products derived from this
+ *     software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY
+ * WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ */
 package net.sf.orc2hdl.design.visitors;
 
 import java.util.ArrayList;
@@ -18,6 +46,14 @@ import net.sf.orcc.ir.InstPhi;
 import net.sf.orcc.ir.Var;
 import net.sf.orcc.ir.util.AbstractIrVisitor;
 
+/**
+ * This visitor figures out the Input and Output of a Branch or a While for an
+ * actor in a SSA form. As for the inputs it is using the BlockVars Visitor and
+ * for the outputs it is using the PH
+ * 
+ * @author Endri Bezati
+ * 
+ */
 public class StmtIO extends AbstractIrVisitor<Void> {
 
 	/** Design Resources **/
