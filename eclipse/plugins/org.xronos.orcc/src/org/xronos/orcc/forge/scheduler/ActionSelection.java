dcb45f28ecfa3fbb5bbe6f078ad0600525146853
endrix <endri.bezati@gmail.com>
2014-07-09 10:11:58 +0200

    (Xronos) NewLimGen, restructuring ActionSelection

--- eclipse/plugins/org.xronos.orcc/src/org/xronos/orcc/forge/scheduler/ActionSelection.java
+++ eclipse/plugins/org.xronos.orcc/src/org/xronos/orcc/forge/scheduler/ActionSelection.java
@@ -55,6 +55,7 @@ import net.sf.orcc.ir.OpBinary;
 import net.sf.orcc.ir.Procedure;
 import net.sf.orcc.ir.Type;
 import net.sf.orcc.ir.Var;
+import net.sf.orcc.ir.util.AbstractIrVisitor;
 import net.sf.orcc.ir.util.IrUtil;
 
 import org.xronos.orcc.ir.BlockMutex;
@@ -66,7 +67,7 @@ import org.xronos.orcc.ir.XronosIrFactory;
  * This visitor construct a list of blocks that handles the action selection
  * 
  * @author Endri Bezati
- *
+ * 
  */
 public class ActionSelection extends DfVisitor<List<Block>> {
 
@@ -79,11 +80,170 @@ public class ActionSelection extends DfVisitor<List<Block>> {
 		this.scheduler = scheduler;
 	}
 
+	public class InputPatternBlocks extends DfVisitor<List<Block>> {
+
+		@Override
+		public List<Block> casePattern(Pattern pattern) {
+			List<Block> blocks = new ArrayList<Block>();
+			return blocks;
+		}
+	}
+
+	public class OutputPatternBlocks extends DfVisitor<List<Block>> {
+		@Override
+		public List<Block> casePattern(Pattern pattern) {
+			List<Block> blocks = new ArrayList<Block>();
+			for (Port port : pattern.getPorts()) {
+				if (pattern.getNumTokens(port) > 1) {
+					BlockBasic blockBasic = IrFactory.eINSTANCE
+							.createBlockBasic();
+					Var target = null;
+					if (scheduler.getLocal(port.getName() + "PortStatus") != null) {
+						target = scheduler.getLocal(port.getName()
+								+ "PortStatus");
+					} else {
+						target = IrFactory.eINSTANCE.createVar(
+								IrFactory.eINSTANCE.createTypeBool(),
+								port.getName() + "PortStatus", true, 0);
+						scheduler.addLocal(target);
+					}
+					// -- Create Port Status
+					InstPortStatus portStatus = XronosIrFactory.eINSTANCE
+							.createInstPortStatus();
+					Def def = IrFactory.eINSTANCE.createDef(target);
+					portStatus.setTarget(def);
+					portStatus.setPort(port);
+					blockBasic.add(portStatus);
+					blocks.add(blockBasic);
+
+					// -- Create Block If
+					Expression condition = IrFactory.eINSTANCE
+							.createExprVar(target);
+					BlockIf blockIf = IrFactory.eINSTANCE.createBlockIf();
+					blockIf.setCondition(condition);
+					// -- Create Block basic for the then Block
+					blockBasic = IrFactory.eINSTANCE.createBlockBasic();
+
+					// -- Create Load and then Port Write
+					if (scheduler.getLocal(port.getName() + "Portvalue") != null) {
+						target = scheduler.getLocal(port.getName()
+								+ "Portvalue");
+					} else {
+						Type type = IrUtil.copy(port.getType());
+						target = IrFactory.eINSTANCE.createVar(type,
+								port.getName() + "Portvalue", true, 0);
+						scheduler.addLocal(target);
+					}
+
+					def = IrFactory.eINSTANCE.createDef(target);
+					Var source = pattern.getPortToVarMap().get(port);
+					InstLoad load = IrFactory.eINSTANCE.createInstLoad(target,
+							source, 0);
+					blockBasic.add(load);
+
+					InstPortWrite portWrite = XronosIrFactory.eINSTANCE
+							.createInstPortWrite();
+					portWrite.setPort(port);
+					Expression value = IrFactory.eINSTANCE
+							.createExprVar(target);
+					portWrite.setValue(value);
+					blockBasic.add(portWrite);
+
+					// -- Add blockBasic to BlockIf then Blocks
+					blockIf.getThenBlocks().add(blockBasic);
+					// -- Add blockIf to Blocks
+					blocks.add(blockIf);
+				} else {
+					// -- Create While Condition
+					Var tokenIndex = scheduler.getLocal(port.getName()
+							+ "TokenIndex");
+					Expression E1 = IrFactory.eINSTANCE
+							.createExprVar(tokenIndex);
+					Expression E2 = IrFactory.eINSTANCE.createExprInt(pattern
+							.getNumTokens(port));
+					Expression condition = IrFactory.eINSTANCE
+							.createExprBinary(E1, OpBinary.LE, E2,
+									IrFactory.eINSTANCE.createTypeBool());
+
+					// -- Create While
+					BlockWhile blockWhile = IrFactory.eINSTANCE
+							.createBlockWhile();
+					blockWhile.setCondition(condition);
+
+					// Create BlockBasic for PinStatus
+					BlockBasic blockBasic = IrFactory.eINSTANCE
+							.createBlockBasic();
+					Var target = null;
+					if (scheduler.getLocal(port.getName() + "PortStatus") != null) {
+						target = scheduler.getLocal(port.getName()
+								+ "PortStatus");
+					} else {
+						target = IrFactory.eINSTANCE.createVar(
+								IrFactory.eINSTANCE.createTypeBool(),
+								port.getName() + "PortStatus", true, 0);
+						scheduler.addLocal(target);
+					}
+					// -- Create Port Status
+					InstPortStatus portStatus = XronosIrFactory.eINSTANCE
+							.createInstPortStatus();
+					Def def = IrFactory.eINSTANCE.createDef(target);
+					portStatus.setTarget(def);
+					portStatus.setPort(port);
+					blockBasic.add(portStatus);
+					blockWhile.getBlocks().add(blockBasic);
+
+					// -- Create Block If
+					condition = IrFactory.eINSTANCE.createExprVar(target);
+					BlockIf blockIf = IrFactory.eINSTANCE.createBlockIf();
+					blockIf.setCondition(condition);
+					// -- Create Block basic for the then Block
+					blockBasic = IrFactory.eINSTANCE.createBlockBasic();
+
+					// -- Create Load and then Port Write
+					if (scheduler.getLocal(port.getName() + "Portvalue") != null) {
+						target = scheduler.getLocal(port.getName()
+								+ "Portvalue");
+					} else {
+						Type type = IrUtil.copy(port.getType());
+						target = IrFactory.eINSTANCE.createVar(type,
+								port.getName() + "Portvalue", true, 0);
+						scheduler.addLocal(target);
+					}
+
+					def = IrFactory.eINSTANCE.createDef(target);
+					Var source = pattern.getPortToVarMap().get(port);
+
+					Expression index = IrFactory.eINSTANCE
+							.createExprVar(tokenIndex);
+					InstLoad load = IrFactory.eINSTANCE.createInstLoad(target,
+							source, Arrays.asList(index));
+					blockBasic.add(load);
+
+					InstPortWrite portWrite = XronosIrFactory.eINSTANCE
+							.createInstPortWrite();
+					portWrite.setPort(port);
+					Expression value = IrFactory.eINSTANCE
+							.createExprVar(target);
+					portWrite.setValue(value);
+					blockBasic.add(portWrite);
+
+					// -- Add blockBasic to BlockIf then Blocks
+					blockIf.getThenBlocks().add(blockBasic);
+					// -- Add to block while blocks
+					blockWhile.getBlocks().add(blockIf);
+					// -- Add to blocks the while block
+					blocks.add(blockWhile);
+				}
+			}
+
+			return blocks;
+		}
+	}
+
 	@Override
 	public List<Block> caseActor(Actor actor) {
 		List<Block> blocks = new ArrayList<Block>();
-		
-		
+
 		// TODO : Create a Block if for each state
 		// FIXME: delete the following code
 		for (Action action : actor.getActions()) {
@@ -129,152 +289,16 @@ public class ActionSelection extends DfVisitor<List<Block>> {
 		// -- Build output pattern while Block
 		if (action.getOutputPattern() != null) {
 			if (action.getOutputPattern().getPorts().size() == 1) {
-				blocks.addAll(doSwitch(action.getOutputPattern()));
+				blocks.addAll(new OutputPatternBlocks().doSwitch(action
+						.getOutputPattern()));
 			} else {
 				BlockMutex mutex = XronosIrFactory.eINSTANCE.createBlockMutex();
-				mutex.getBlocks().addAll(doSwitch(action.getOutputPattern()));
+				mutex.getBlocks().addAll(
+						new OutputPatternBlocks().doSwitch(action
+								.getOutputPattern()));
 				blocks.add(mutex);
 			}
 		}
-		return blocks;
-	}
-
-	@Override
-	public List<Block> casePattern(Pattern pattern) {
-		List<Block> blocks = new ArrayList<Block>();
-		for (Port port : pattern.getPorts()) {
-
-			if (pattern.getNumTokens(port) > 1) {
-				BlockBasic blockBasic = IrFactory.eINSTANCE.createBlockBasic();
-				Var target = null;
-				if (scheduler.getLocal(port.getName() + "PortStatus") != null) {
-					target = scheduler.getLocal(port.getName() + "PortStatus");
-				} else {
-					target = IrFactory.eINSTANCE.createVar(
-							IrFactory.eINSTANCE.createTypeBool(),
-							port.getName() + "PortStatus", true, 0);
-					scheduler.addLocal(target);
-				}
-				// -- Create Port Status
-				InstPortStatus portStatus = XronosIrFactory.eINSTANCE
-						.createInstPortStatus();
-				Def def = IrFactory.eINSTANCE.createDef(target);
-				portStatus.setTarget(def);
-				portStatus.setPort(port);
-				blockBasic.add(portStatus);
-				blocks.add(blockBasic);
-
-				// -- Create Block If
-				Expression condition = IrFactory.eINSTANCE
-						.createExprVar(target);
-				BlockIf blockIf = IrFactory.eINSTANCE.createBlockIf();
-				blockIf.setCondition(condition);
-				// -- Create Block basic for the then Block
-				blockBasic = IrFactory.eINSTANCE.createBlockBasic();
-
-				// -- Create Load and then Port Write
-				if (scheduler.getLocal(port.getName() + "Portvalue") != null) {
-					target = scheduler.getLocal(port.getName() + "Portvalue");
-				} else {
-					Type type = IrUtil.copy(port.getType());
-					target = IrFactory.eINSTANCE.createVar(type, port.getName()
-							+ "Portvalue", true, 0);
-					scheduler.addLocal(target);
-				}
-
-				def = IrFactory.eINSTANCE.createDef(target);
-				Var source = pattern.getPortToVarMap().get(port);
-				InstLoad load = IrFactory.eINSTANCE.createInstLoad(target,
-						source, 0);
-				blockBasic.add(load);
-
-				InstPortWrite portWrite = XronosIrFactory.eINSTANCE
-						.createInstPortWrite();
-				portWrite.setPort(port);
-				Expression value = IrFactory.eINSTANCE.createExprVar(target);
-				portWrite.setValue(value);
-				blockBasic.add(portWrite);
-
-				// -- Add blockBasic to BlockIf then Blocks
-				blockIf.getThenBlocks().add(blockBasic);
-				// -- Add blockIf to Blocks
-				blocks.add(blockIf);
-			} else {
-				// -- Create While Condition
-				Var tokenIndex = scheduler.getLocal(port.getName()
-						+ "TokenIndex");
-				Expression E1 = IrFactory.eINSTANCE.createExprVar(tokenIndex);
-				Expression E2 = IrFactory.eINSTANCE.createExprInt(pattern
-						.getNumTokens(port));
-				Expression condition = IrFactory.eINSTANCE.createExprBinary(E1,
-						OpBinary.LE, E2, IrFactory.eINSTANCE.createTypeBool());
-
-				// -- Create While
-				BlockWhile blockWhile = IrFactory.eINSTANCE.createBlockWhile();
-				blockWhile.setCondition(condition);
-
-				// Create BlockBasic for PinStatus
-				BlockBasic blockBasic = IrFactory.eINSTANCE.createBlockBasic();
-				Var target = null;
-				if (scheduler.getLocal(port.getName() + "PortStatus") != null) {
-					target = scheduler.getLocal(port.getName() + "PortStatus");
-				} else {
-					target = IrFactory.eINSTANCE.createVar(
-							IrFactory.eINSTANCE.createTypeBool(),
-							port.getName() + "PortStatus", true, 0);
-					scheduler.addLocal(target);
-				}
-				// -- Create Port Status
-				InstPortStatus portStatus = XronosIrFactory.eINSTANCE
-						.createInstPortStatus();
-				Def def = IrFactory.eINSTANCE.createDef(target);
-				portStatus.setTarget(def);
-				portStatus.setPort(port);
-				blockBasic.add(portStatus);
-				blockWhile.getBlocks().add(blockBasic);
-
-				// -- Create Block If
-				condition = IrFactory.eINSTANCE.createExprVar(target);
-				BlockIf blockIf = IrFactory.eINSTANCE.createBlockIf();
-				blockIf.setCondition(condition);
-				// -- Create Block basic for the then Block
-				blockBasic = IrFactory.eINSTANCE.createBlockBasic();
-
-				// -- Create Load and then Port Write
-				if (scheduler.getLocal(port.getName() + "Portvalue") != null) {
-					target = scheduler.getLocal(port.getName() + "Portvalue");
-				} else {
-					Type type = IrUtil.copy(port.getType());
-					target = IrFactory.eINSTANCE.createVar(type, port.getName()
-							+ "Portvalue", true, 0);
-					scheduler.addLocal(target);
-				}
-
-				def = IrFactory.eINSTANCE.createDef(target);
-				Var source = pattern.getPortToVarMap().get(port);
-
-				Expression index = IrFactory.eINSTANCE
-						.createExprVar(tokenIndex);
-				InstLoad load = IrFactory.eINSTANCE.createInstLoad(target,
-						source, Arrays.asList(index));
-				blockBasic.add(load);
-
-				InstPortWrite portWrite = XronosIrFactory.eINSTANCE
-						.createInstPortWrite();
-				portWrite.setPort(port);
-				Expression value = IrFactory.eINSTANCE.createExprVar(target);
-				portWrite.setValue(value);
-				blockBasic.add(portWrite);
-
-				// -- Add blockBasic to BlockIf then Blocks
-				blockIf.getThenBlocks().add(blockBasic);
-				// -- Add to block while blocks
-				blockWhile.getBlocks().add(blockIf);
-				// -- Add to blocks the while block
-				blocks.add(blockWhile);
-			}
-		}
-
 		return blocks;
 	}
 
